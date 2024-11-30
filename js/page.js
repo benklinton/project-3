d2d0b6a8a52da975094abaaa2f5282c1c0de2c78
Ben Linton <benklinton@gmail.com>
2024-11-29 22:24:05 -0700

    added form submission

--- js/page.js
+++ js/page.js
@@ -1,3 +1,20 @@
+/* 
+ 
+Project:  Project 5 Vistor Form Validation
+Name: Ben Linton
+Submitted: 12/02/2024
+ 
+I declare that the following source code was written by me, or provided
+by the instructor for this project. I understand that copying source
+code from any other source, providing source code to another student, 
+or leaving my code on a public web site constitutes cheating.
+I acknowledge that  If I am found in violation of this policy this may result
+in a zero grade, a permanent record on file and possibly immediate failure of the class.
+ 
+Reflection (1-2 paragraphs):  I really enjoyed writing this program, but had trouble with blah blah blah.  
+ 
+*/
+
 $(document).ready(function () {
     $('.slider').slick({
         dots: true,
@@ -41,20 +58,29 @@ $(document).ready(function () {
         $("#film-title").css('display', 'none');
         $("#film-cards").css('display', 'none');
         $("#trivia").css('display', 'none');
+        $("#form").css('display', 'none');
     });
     $('#trivia-nav').click(function () {
         $('#trivia').css('display', 'flex');
         $("#film-title").css('display', 'none');
         $("#film-cards").css('display', 'none');
         $("#home").css('display', 'none');
+        $("#form").css('display', 'none');
     });
     $('#films-nav').click(function () {
         $('#film-title').css('display', 'flex');
         $("#film-cards").css('display', 'flex');
         $("#home").css('display', 'none');
         $("#trivia").css('display', 'none');
+        $("#form").css('display', 'none');
     });
-
+    $('#form-nav').click(function () {
+        $('#form').css('display', 'block');
+        $("#film-title").css('display', 'none');
+        $("#film-cards").css('display', 'none');
+        $("#home").css('display', 'none');
+        $("#trivia").css('display', 'none');
+    });
     $('.start-button').click(function () {
         $('.start-button').hide();
         $('.trivia-header').hide();
---
+++ js/validation.js
@@ -0,0 +1,126 @@
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation')
+  const state = document.querySelector('input[name="state"]');
+  const zip = document.querySelector('input[name="zip"]');
+  const email = document.querySelector('input[name="email"]');
+  const phone = document.querySelector('input[name="phone"]');
+
+  const validStates = [
+    'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'
+  ];
+
+  const validateState = (state) => {
+    return validStates.includes(state.value.toUpperCase());
+  }
+  const validateZip = (zip) => {
+    const zipPattern = /^[0-9]{5}$/;
+    return zipPattern.test(zip.value);
+  }
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email.value);
+  }
+
+  const validatePhone = (phone) => {
+    const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+    return phonePattern.test(phone.value);
+  }
+
+  $(state).on('input', function() {
+    if (state && !validateState(state)) {
+      $('.state-feedback').text('Please enter a valid state abbreviation.');
+      state.classList.remove('is-valid')
+      state.classList.add('is-invalid')
+    }
+    else {
+      state.classList.remove('is-invalid');
+      state.classList.add('is-valid');
+    }
+  });
+
+  $(zip).on('input', function() {
+    if (zip && !validateZip(zip)) {
+      $('.zip-feedback').text('Please enter a valid zip code.');
+      zip.classList.remove('is-valid')
+      zip.classList.add('is-invalid')
+    }
+    else {
+      zip.classList.remove('is-invalid');
+      zip.classList.add('is-valid');
+    }
+  });
+
+ $(email).on('input', function() {
+    if (email && !validateEmail(email)) {
+      $('.email-feedback').text('Please enter a valid email address.');
+    }
+    else {
+      email.classList.remove('is-invalid');
+      email.classList.add('is-valid');
+    }
+  });
+
+  $(phone).on('input', function() {
+    if (phone && !validatePhone(phone)) {
+      $('.phone-feedback').text('Format must be ###-###-####.');
+     phone.classList.remove('is-valid')
+      phone.classList.add('is-invalid')
+    }
+    else {
+      phone.classList.remove('is-invalid');
+      phone.classList.add('is-valid');
+    }
+  });
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      let isValid = true;
+      const state = form.querySelector('input[name="state"]');
+      const email = form.querySelector('input[name="email"]');
+      const phone = form.querySelector('input[name="phone"]');
+      const zip = form.querySelector('input[name="zip"]');
+
+      if (state && !validateState(state)) {
+        isValid = false;
+      } else {
+        state.classList.remove('is-invalid');
+      }
+      if (zip && !validateZip(zip)) {
+        isValid = false;
+      } else {
+        zip.classList.remove('is-invalid');
+      }
+      if (email && !validateEmail(email)) {
+        isValid = false;
+      } else {
+        email.classList.remove('is-invalid');
+      }
+      if (phone && !validatePhone(phone)) {
+        isValid = false;
+        phone.classList.remove('is-valid')
+        phone.classList.add('is-invalid')
+      }
+      else {
+        phone.classList.remove('is-invalid');
+        phone.classList.add('is-valid');
+      }
+      if (!form.checkValidity() || !isValid) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      else {
+        event.preventDefault();
+        $("#form").css('display', 'none');
+        $("#form-confirmation").css('display', 'block');
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
